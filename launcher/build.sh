85afac06fd628b7a981db7963332312c04f77fd5
Jayesh M. Prajapati <jayesh.prajapati@smartstream-stp.com>
2019-07-19 17:16:52 +0530

    Added launcher project where Statements Manager UI can be opened instead of opening it in browser

---
+++ launcher/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mvn clean install -s settings.xml $*
---
+++ launcher/src/main/java/outil/fin/Launcher.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package outil.fin;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTError;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * @author Prajapati
+ *
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+		final Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText(/*"Snippet 148"*/"SWT Browser");
+		shell.setLayout(new FillLayout());
+		Browser browser = null;
+		try {
+			browser = new Browser(shell, SWT.NONE);
+		} catch (SWTError e) {
+			/* The Browser widget throws an SWTError if it fails to
+			 * instantiate properly. Application code should catch
+			 * this SWTError and disable any feature requiring the
+			 * Browser widget.
+			 * Platform requirements for the SWT Browser widget are available
+			 * from the SWT FAQ website.
+			 */
+		}
+		if (browser != null) {
+			/* The Browser widget can be used */
+			browser.setUrl("http://localhost:8080/wicket");
+		}
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+	}
+}

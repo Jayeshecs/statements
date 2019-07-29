7f52b8e0e8c197c6ac188c74063b72ff0a0cc035
Jayesh M. Prajapati <jayesh.prajapati@smartstream-stp.com>
2019-07-29 12:28:37 +0530

    Updated start.sh file to respect argument 'headless'

--- start.sh
+++ start.sh
@@ -2,7 +2,12 @@
 echo copying standalong statements.war file...
 cp webapp/target/statements-webapp-*-jetty*.war ./statements.war
 echo Start statements application ...
-nohup java -jar statements.war --headless 2>&1 > logs/console.log &
+if [ "$1" == "headless" ] 
+then
+	nohup java -jar statements.war --headless 2>&1 > logs/console.log &
+else
+	nohup java -jar statements.war 2>&1 > logs/console.log &
+fi
 echo To see progress make use of below command
 echo tail -f logs/console.log
 
